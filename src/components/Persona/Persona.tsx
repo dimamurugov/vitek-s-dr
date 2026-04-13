491cb48c64fc9fb14bd82e5f3beca8e8a39db86e
Dim_Legion <dimamurugov@gmail.com>
2026-04-13 22:47:14 +0300

    add background

--- src/components/Persona/Persona.tsx
+++ src/components/Persona/Persona.tsx
@@ -1,9 +1,11 @@
 import vitekSrc from './vitek.svg'
+import holmBg from './holm.png'
 import './Persona.css'
 
 export function Persona() {
   return (
     <section className="persona" aria-label="Persona">
+      <img className="persona-bg" src={holmBg} alt="" decoding="async" />
       <img className="persona-svg" src={vitekSrc} alt="" decoding="async" />
     </section>
   )
